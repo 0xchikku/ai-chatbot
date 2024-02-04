a39c56daff310e88c73603faf85d9fa49732dc48
Ritesh Dubey <0xchikku@gmail.com>
2024-02-04 11:52:08 +0530

    setting up express

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,10 @@
-console.log('Hello, Typescript');
-export {};
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get('/user/:userId', (req, res, next) => {
+    console.log("🚀 ~ app.get ~ req:", req.params.userId);
+    console.log('/hello - called');
+    return res.send("Hello");
+});
+app.listen(5001, () => console.log('Server Open'));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, Typescript')
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get('/user/:userId', (req, res, next) => {
+  console.log("🚀 ~ app.get ~ req:", req.params.userId)
+  console.log('/hello - called');
+  return res.send("Hello")
+})
+app.listen(5001, () => console.log('Server Open'))
