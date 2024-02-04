4a0777014b98c4e2386ea97c6af208a3d928dfbc
Ritesh Dubey <0xchikku@gmail.com>
2024-02-04 13:18:02 +0530

    get all user route and controller

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,12 @@
-export const getAllUsers = () => {
-    // get all users
-    console.log("get all users");
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(`Error while getting all users, error: ${error}`);
+        return res.status(500).json({ message: "ERROR", cause: (error.message) });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
