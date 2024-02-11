4a0410b2af602a7f3b3e95d50421f33174d3d5de
Ritesh Dubey <0xchikku@gmail.com>
2024-02-11 08:48:26 +0530

    auth content and provider setup

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+
+type User = {
+  name: string,
+  email: string,
+}
+
+type UserAuth = {
+  isLoggedIn: boolean,
+  user: User | null,
+  login: (email: string, password: string) => Promise<void>,
+  signup: (email: string, name: string, password: string) => Promise<void>,
+  logout: () => Promise<void>
+}
+
+const AuthContext = createContext<UserAuth | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  useEffect(() => { }, []);
+
+  const login = (email: string, password: string) => { };
+  const signup = (email: string, name: string, password: string) => { };
+  const logout = () => { }
+
+  const value = {
+    user,
+    isLoggedIn,
+    login,
+    signup,
+    logout,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => useContext(AuthContext);
